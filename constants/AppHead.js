faa6a2ce7f8feed6c938adf5bb047b8b1d3d1caf
animeshgarai09 <animeshgarai09@gmail.com>
2021-07-12 22:19:59 +0530

    Meta data changes

--- constants/AppHead.js
+++ constants/AppHead.js
@@ -3,11 +3,11 @@ const AppHead = ({ title }) => {
     return (
         <>
             <Head>
-                <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
-                <link rel="manifest" href="favicon/site.webmanifest" />
-                <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#621a9a" />
+                <link rel="apple-touch-icon" sizes="180x180" href="../public/favicon/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon/favicon-16x16.png" />
+                <link rel="manifest" href="../public/favicon/site.webmanifest" />
+                <link rel="mask-icon" href="../public/favicon/safari-pinned-tab.svg" color="#621a9a" />
                 <meta name="apple-mobile-web-app-title" content="Animeshdesigns" />
                 <meta name="application-name" content="Animeshdesigns" />
                 <meta name="msapplication-TileColor" content="#ffc40d" />
