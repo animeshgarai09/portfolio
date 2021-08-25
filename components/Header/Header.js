0480263fe5757c7edf932f3ff23e8edf53b5aad6
animeshgarai09 <animeshgarai09@gmail.com>
2021-08-25 16:33:14 +0530

    Menu animation done

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -8,21 +8,28 @@ import ReactTooltip from 'react-tooltip';
 import { HiOutlineMail } from 'react-icons/hi'
 import { RiTwitterLine, RiLinkedinLine } from 'react-icons/ri'
 import { FiGithub } from 'react-icons/fi'
-import { CgMenuRightAlt, CgClose } from 'react-icons/cg'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger.js'
 import { FiSmile, FiFeather, FiLayers, FiMessageCircle, FiFlag, FiArrowUp } from "react-icons/fi";
 import { HiOutlinePencil } from "react-icons/hi";
-const Header = ({ refs, mobNavRef, mbload }) => {
+
+const Header = ({ refs, mobNavRef }) => {
     const darkMode = useDarkMode();
     const [mount, setMount] = useState(false);
+    const [navState, setNavState] = useState(false)
     const navAnimation = useRef()
-    const openNav = () => {
-        // document.body.style.overflowY = 'hidden'
-        document.documentElement.style.overflowY = 'hidden'
-        // setMbMenu(true)
-        // gsap.set('.mb_nav_btn', { clearProps: "all" })
-        console.log('inside')
-        navAnimation.current.play()
+
+    const navToggle = () => {
+
+        if (!navState) {
+            document.documentElement.style.overflowY = 'hidden'
+            console.log('inside')
+            navAnimation.current.timeScale(1).play()
+        } else {
+            document.documentElement.style.overflowY = 'overlay'
+            console.log('inside')
+            navAnimation.current.timeScale(2).reverse()
+        }
+        setNavState(!navState)
     }
     useEffect(() => {
         const media = window.matchMedia('(max-width:850px)')
@@ -34,14 +41,28 @@ const Header = ({ refs, mobNavRef, mbload }) => {
         media.addListener((e) => handleMatch(e.matches));
         handleMatch(media.matches);
 
-        // gsap.set(`.${dropNav}`, {
-        //     css:{
-        //         filter:"blur(0px)"
-        //     }
-        // })
+
         navAnimation.current = gsap.timeline({ paused: true })
-        navAnimation.current.from(`.${styles.dropNav}`, {
-            autoAlpha: 1
+        navAnimation.current.to(`.${styles.dropNav_con}`, {
+            autoAlpha: 1,
+            duration: 0.5,
+            ease: "power4.out"
+        }).from(`.${styles.dropNav}`, {
+            yPercent: -100,
+            duration: 0.7,
+            delay: -0.5,
+            ease: "power4.out"
+        }).from(`.mb_nav_btn`, {
+            scale: 0,
+            autoAlpha: 0,
+            ease: "power3.out",
+            stagger: 0.1,
+            delay: -0.3
+        }).from(`.${styles.mb_links}`, {
+            scale: 0.5,
+            autoAlpha: 0,
+            ease: "power3.out",
+            duration: 0.2,
         })
 
     }, [])
@@ -127,7 +148,13 @@ const Header = ({ refs, mobNavRef, mbload }) => {
                             onChange={darkMode.toggle}
                         />
                         <label htmlFor={styles.themeSwitcher} className='mb_nav'></label>
-                        <button onClick={openNav} className='mb_nav'><CgMenuRightAlt /></button>
+                        {/* <button onClick={openNav} className='mb_nav'><CgMenuRightAlt /></button> */}
+                        <div onClick={navToggle} className={styles.menu + ` ${navState ? styles.open : ''}`}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -157,60 +184,7 @@ const Header = ({ refs, mobNavRef, mbload }) => {
     );
 }
 
-const DropNavOld = ({ refs, mobNavRef }) => {
 
-    const closeNav = () => {
-        document.documentElement.style.overflowY = 'overlay'
-        const mbAnimation = gsap.timeline()
-        mbAnimation.to('.mb_nav_btn', {
-            x: 20,
-            autoAlpha: 0,
-            duration: 1,
-            stagger: 0.1,
-            // delay: -0.3,
-            ease: "expo.out(0.9, 0.1)"
-        }).to('#mb_wrapper', {
-            scale: .7,
-            autoAlpha: 0,
-            ease: 'power4.in',
-            duration: .3,
-            delay: -1.2
-        })
-        // open(false)
-    }
-    return (
-        <div className={styles.wrapper} >
-            <div className={styles.dropNav_con} id="mb_wrapper">
-                <div className={styles.top}>
-                    <span><CgMenuRightAlt />Menu</span>
-                    <button onClick={closeNav}><CgClose /></button>
-                </div>
-                <div className={styles.nav}>
-                    <ul id="mb_nav_ul">
-                        <li className='mb_nav_btn' onClick={() => scroll(refs.current[0], 1)}><a id="1" ref={(el) => mobNavRef.current.push(el)} >About</a></li>
-                        <li className='mb_nav_btn' onClick={() => scroll(refs.current[1], 2)}><a id="2" ref={(el) => mobNavRef.current.push(el)} >Career</a></li>
-                        <li className='mb_nav_btn' onClick={() => scroll(refs.current[2], 3)}><a id="3" ref={(el) => mobNavRef.current.push(el)} >Skills</a></li>
-                        <li className='mb_nav_btn' onClick={() => scroll(refs.current[3], 4)}><a id="4" ref={(el) => mobNavRef.current.push(el)} >Portfolio</a></li>
-                        <li className='mb_nav_btn' onClick={() => scroll(refs.current[4], 5)}><a id="5" ref={(el) => mobNavRef.current.push(el)} >Blogs</a></li>
-                        <li className='mb_nav_btn' onClick={() => scroll(refs.current[5], 6)}><a id="6" ref={(el) => mobNavRef.current.push(el)} >Contact</a></li>
-                    </ul>
-                    <div className={styles.mb_links}>
-                        <Link href="https://www.linkedin.com/in/animesh-garai-29a5251b4">
-                            <a target="_blank" className='mb-link'><RiLinkedinLine /></a>
-                        </Link>
-                        {/* <Link href="/projects"> */}
-                        <a className='mb-link'><FiGithub /></a>
-                        <a className='mb-link'><RiTwitterLine /></a>
-                        <a className='mb-link'><HiOutlineMail /></a>
-                    </div>
-                </div>
-                <div className={styles.social}>
-
-                </div>
-            </div>
-        </div>
-    )
-}
 export default Header
 
 
